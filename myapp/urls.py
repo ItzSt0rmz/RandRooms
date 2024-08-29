ecdd10a260646fe8106719ab6b6903edb3daa022
Cole Hawkins <120764143+ItzSt0rmz@users.noreply.github.com>
2024-08-29 15:53:49 -0500

    Moving code into GitHub

---
+++ myapp/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path("", views.home, name ="home"),
+    path("home/", views.home, name="home"),
+    path("todos/", views.todos, name="todos"),
+    path("checkers/", views.checkers, name="checkers"),
+    path("register/", views.registerPage, name="register"),
+    path("login/", views.loginPage, name ="login"),
+    path("logout/", views.logOutUser, name ="logout"),
+]
